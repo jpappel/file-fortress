c2d3c2e7ee4d9864f268dbd325dcc3000939d6a5
JP Appel <jeanpierre.appel01@gmail.com>
2023-11-12 14:36:42 -0500

    add procedure to remove files after expiration
    Create stored procedure `RemoveExpired` to remove files
    after a given date. Has a return value that stores how many
    files were removed.

---
+++ mariadb/procedures.sql
@@ -0,0 +1,32 @@
+USE filefort;
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE RemoveExpired (IN p_expire_date DATETIME, OUT p_remove_count INT)
+BEGIN
+    DECLARE v_error_message VARCHAR(255);
+    DECLARE exit_handler BOOLEAN DEFAULT FALSE;
+
+    -- Declare the handler for SQL exceptions
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        SET exit_handler = TRUE;
+
+    START TRANSACTION;
+
+    DELETE FROM files
+    WHERE expires < p_expire_date;
+
+    -- Get the number of rows deleted
+    SELECT ROW_COUNT() INTO p_remove_count;
+
+    IF NOT exit_handler THEN
+        COMMIT;
+    ELSE
+        ROLLBACK;
+        -- Log the error
+        SELECT CONCAT('Error in RemoveExpired procedure: ', ERROR_MESSAGE()) INTO v_error_message;
+    END IF;
+END;
+//
+
+DELIMITER ;
