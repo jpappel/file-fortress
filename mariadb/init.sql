d587da4ba84ae2fcf81751bd064ac62695142ed4
JP Appel <jeanpierre.appel01@gmail.com>
2023-11-12 14:08:06 -0500

    add indices to permissions table

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -20,7 +20,7 @@ CREATE TABLE files (
     url VARCHAR(255),
     mime_type VARCHAR(31),
     expires DATETIME,
-    privacy ENUM('public', 'share', 'private') DEFAULT 'public',
+    privacy ENUM('public', 'share', 'private') DEFAULT 'public' NOT NULL,
     PRIMARY KEY (id),
     INDEX(uploader_id, mime_type),
     FOREIGN KEY (uploader_id) REFERENCES users(id)
@@ -30,6 +30,9 @@ CREATE TABLE permissions (
     file_id INT UNSIGNED,
     user_id UUID,
     permission ENUM('owner', 'view'),
+    INDEX(file_id),
+    INDEX(user_id),
+    INDEX(user_id, permission),
     FOREIGN KEY (file_id) REFERENCES files(id)
         ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id)
