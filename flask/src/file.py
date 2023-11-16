6c60f41cfe0848bb3217a95e5827aae05247433e
Owen Halliday <hallidayo@moravian.edu>
2023-11-15 21:24:06 -0500

    fixed crash on endpoint
    it wasn't as much of a drop in replacement as i thought, it required a little bit more.

--- flask/src/file.py
+++ flask/src/file.py
@@ -1,8 +1,8 @@
 from db import get_db
 
 def get_file(short_link):
-    db = get_db()
-    db.execute("SELECT * FROM files WHERE short_link=?", (short_link,))
+    db = get_db().cursor()
+    db.execute("SELECT * FROM files WHERE short_link=%s", (short_link,))
     file = db.fetchone()
     if not file:
         return None
