fffe6be22bd15440f693d708fadd260b499bbffa
Owen Halliday <hallidayo@moravian.edu>
2023-11-16 12:54:59 -0500

    fixed mocking

--- flask/tests/test_file_routes.py
+++ flask/tests/test_file_routes.py
@@ -11,29 +11,19 @@ def client():
         yield client
 
 
-    
-@pytest.fixture
-def mock_file_not_found(mocker):
-    get_file_mock = mocker.patch('file.get_file', return_value=None)
-    yield get_file_mock
+def test_file_not_found(client,mocker):
+    # mock the value from get_file
+    mocked_get_file = mocker.patch('src.file_routes.get_file')
+    mocked_get_file.return_value = None
 
-def test_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    
-    assert b"Hello World!" in response.data
-
-def test_file_not_found(mocker):
-    # mock the get_file function
-    mocker.patch('file.get_file', return_value=None)
-
-    response = client.get("/test.png")
+    response = client.get("/api/v1/file/test.png")
     assert response.status_code == 404
     assert {'error': 'file not found'} == response.json
 
-def test_file_found(mocker):
-    # mock the get_file function
-    mocker.patch('file.get_file', return_value={
+def test_file_found(client, mocker):
+    #mock the value from get_file
+    mocked_get_file = mocker.patch('src.file_routes.get_file')
+    expected_return = {
         "id": 1,
         "uploader_id": str(uuid.uuid4()),
         "short_link": "test.png",
@@ -43,18 +33,9 @@ def test_file_found(mocker):
         "privacy": "public",
         "modified_date": "2021-01-01 00:00:00",
         "created_date": "2021-01-01 00:00:00"
-    })
+    }
 
-    response = client.get("/test.png")
+    mocked_get_file.return_value = expected_return
+    response = client.get("/api/v1/file/test.png")
     assert response.status_code == 200
-    assert {
-        "id": 1,
-        "uploader_id": mock_file_found.return_value["uploader_id"],
-        "short_link": "test.png",
-        "url": "https://example.com/test.png",
-        "mime_type": "image/png",
-        "expires": None,
-        "privacy": "public",
-        "modified_date": "2021-01-01 00:00:00",
-        "created_date": "2021-01-01 00:00:00"
-    } == response.json
+    assert expected_return == response.json
