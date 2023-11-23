c25684df57170f5462968421e2dd0374fcc21bd3
JP Appel <jeanpierre.appel01@gmail.com>
2023-11-22 20:09:47 -0500

    add tests for collections

--- mariadb/tests/test_database.py
+++ mariadb/tests/test_database.py
@@ -44,6 +44,7 @@ def sample_users(connection, cursor) -> list:
 
 
 def empty_db(connection, cursor) -> None:
+    cursor.execute('DELETE FROM collections')
     cursor.execute('DELETE FROM files')
     cursor.execute('DELETE FROM users')
     connection.commit()
@@ -67,12 +68,37 @@ def test_uploader_gets_owner_permissions(connection, cursor):
     cursor.execute('SELECT file_id, user_id FROM permissions WHERE permission = "owner"')
     file_owners = dict(cursor.fetchall())
 
-    assert file_uploaders == file_owners, f"file uploaders were not given file owner permissions"
+
+    cursor.execute('SELECT * FROM permissions')
+
+    assert file_uploaders == file_owners, "file uploaders were not given file owner permissions"
 
     connection.rollback()
 
 
-def test_remove_expired(connection, cursor):
+def test_collection_creator_gets_owner_permissions(connection, cursor):
+    empty_db(connection, cursor)
+    user = sample_users(connection, cursor)[0]
+
+    # insert collection
+    collection = (user, 'collection name', '1970-01-01')
+    insert_collection = 'INSERT INTO collections (creator_id, name, expires) VALUES (%s, %s, %s)'
+    cursor.execute(insert_collection, collection)
+
+    cursor.execute('SELECT id FROM collections WHERE creator_id = %s', user)
+    collection_id = cursor.fetchone()[0]
+
+    collection_uploaders = {collection_id: user}
+
+    cursor.execute('SELECT collection_id, user_id FROM collection_permissions WHERE permission = "owner"')
+    collection_owners = dict(cursor.fetchall())
+
+    assert collection_uploaders == collection_owners, "collection uploaders were not given collection owner permissions"
+
+    connection.rollback()
+
+
+def test_remove_expired_files(connection, cursor):
     empty_db(connection, cursor)
     user_ids = sample_users(connection, cursor)
 
@@ -82,8 +108,42 @@ def test_remove_expired(connection, cursor):
     cursor.executemany(query, expired_files)
     connection.commit()
 
-    cursor.callproc('RemoveExpired', ('2023-11-17', 0))
+    cursor.callproc('RemoveExpiredFiles', ('2023-11-17', 0))
     # NOTE: kind of hacky, pymysql was not playing nice with output from a stored procedure
     removed_files = cursor.rowcount - 1
 
     assert len(expired_files) == removed_files, f'number of removed files ({removed_files}) does not match the number of expired files ({len(expired_files)})'
+
+
+def test_remove_expired_collections(connection, cursor):
+    empty_db(connection, cursor)
+    user = sample_users(connection, cursor)[0]
+
+    # insert test file
+    file = (user, 'shortlink', 'url', 'mime', '1970-01-01', 'public')
+    insert_file = 'INSERT INTO files(uploader_id, short_link, url, mime_type, expires, privacy) VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(insert_file, file)
+    connection.commit()
+
+    # insert collection
+    collection = (user, 'collection name', '1970-01-01')
+    insert_collection = 'INSERT INTO collections (creator_id, name, expires) VALUES (%s, %s, %s)'
+    cursor.execute(insert_collection, collection)
+    connection.commit()
+
+    # insert test file into collecton
+    cursor.execute('SELECT id FROM files WHERE uploader_id = %s', user)
+    file_id = cursor.fetchone()[0]
+
+    cursor.execute('SELECT id FROM collections WHERE creator_id = %s', user)
+    collection_id = cursor.fetchone()[0]
+
+    insert_collection_file = 'INSERT INTO collection_files VALUES (%s, %s)'
+    cursor.execute(insert_collection_file, (collection_id, file_id))
+    connection.commit()
+
+    # NOTE: same hack as above
+    cursor.callproc('RemoveExpiredCollections', ('2023-11-17', 0))
+    removed_collections = cursor.rowcount - 1
+
+    assert removed_collections == 1
