38adba45a0352a611e5ceb8345be3edf5a66fc8c
JP Appel <jeanpierre.appel01@gmail.com>
2023-12-06 10:44:36 -0500

    created app factory in module __init__

--- flask/src/__init__.py
+++ flask/src/__init__.py
@@ -1,1 +1,31 @@
-from . import app
+from flask import Flask
+from .file_routes import file_api, file_view
+from .website_routes import website
+from .users import users, login_manager
+from .configurations import Config
+
+
+def create_app(config: Config = None) -> Flask:
+    app = Flask(__name__, template_folder="./templates", static_folder="./static")
+
+    if config is None:
+        from .configurations import production_config
+        app.config.from_object(production_config())
+    else:
+        app.config.from_object(config)
+
+    # init login manager
+    login_manager.init_app(app)
+
+    # register blueprints
+    app.register_blueprint(file_api)
+    app.register_blueprint(website)
+    app.register_blueprint(users)
+    app.register_blueprint(file_view)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
--- flask/src/configurations.py
+++ flask/src/configurations.py
@@ -3,14 +3,25 @@ from .StorageManagers import LocalStorageManager
 
 
 class Config(object):
+    """
+    Base Configuration for File Fortress
+    """
+    DEBUG = False
     TESTING = False
+    # FIXME: generate a random secret key
+    SECRET_KEY = 'demo_secret_key'
+    DB = None
 
 
 class TestConfig(Config):
+    DEBUG = True
     TESTING = True
 
 
-class ProductionConfig(Config):
-    DB = get_db()
-    STORAGE_ROOT = '/mnt/file_storage'
-    STORAGE_MANAGER = LocalStorageManager(DB, STORAGE_ROOT)
+def production_config() -> Config:
+    class ProductionConfig(Config):
+        DB = get_db()
+        STORAGE_ROOT = '/mnt/file_storage'
+        STORAGE_MANAGER = LocalStorageManager(DB, STORAGE_ROOT)
+
+    return ProductionConfig()
